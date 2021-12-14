de3e73194cb51d620acde1ba38534f98f45c1c10
jegorjegor <egorchuzhavko@gmail.com>
2021-12-14 05:21:52 +0300

    undasx

---
+++ nasledovanieinterfeici/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nasledovanieinterfeici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
